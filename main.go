58306a93bf526f739b6a950745225ca4bd634a56
Tiago Jose <tiago_jvo@outlook.com>
2021-01-31 23:38:37 -0300

    done

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	FirstName string   `json:"firstName,omitempty"`
+	LastName  string   `json:"lastNanem,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+func GetPersonEndPoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+func GetPeopleEndPoint(w http.ResponseWriter, req *http.Request) {
+
+	json.NewEncoder(w).Encode(people)
+}
+
+func CreatePersonEndPoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	var person Person
+	_ = json.NewDecoder(req.Body).Decode(&person)
+	person.ID = params["ID"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+func DeletePersonEndPoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+
+	json.NewEncoder(w).Encode(people)
+}
+
+func main() {
+	router := mux.NewRouter()
+	people = append(people, Person{ID: "1", FirstName: "Tiago", LastName: "José", Address: &Address{City: "JP", State: "RN"}})
+	people = append(people, Person{ID: "2", FirstName: "Ayla", LastName: "Nalanda"})
+	router.HandleFunc("/people", GetPeopleEndPoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndPoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndPoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndPoint).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
